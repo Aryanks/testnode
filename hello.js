8630570fae0d155b19a7df358a4d950e697ae7b4
Ubuntu <ubuntu@ip-172-31-16-115.us-east-2.compute.internal>
2019-01-07 06:21:19 +0000

    new chaneds

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World\test\n');
 }).listen(8080, '172.31.16.115');
 console.log('Server running at http://172.31.16.115:8080/');
