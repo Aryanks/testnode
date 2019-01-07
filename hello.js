cee1b02b0ecc85505d6cf9f3e15e320fe30fe72c
root <root@ip-172-31-16-115.us-east-2.compute.internal>
2019-01-07 13:09:05 +0000

    jjjud

--- hello.js
+++ hello.js
@@ -2,6 +2,11 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\test666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666\n');
-}).listen(8080, '172.31.16.115');
-console.log('Server running at http://172.31.16.115:8080/');
+  res.end('Hello World\test66666666\n');
+}).listen(8081, '172.31.16.115');
+console.log('Server running at http://172.31.16.115:8081/');
+
+#var forward = require('http-port-forward');
+
+#// forward all local 8080 port http requests to 80 port.
+#forward(8080, 80);
